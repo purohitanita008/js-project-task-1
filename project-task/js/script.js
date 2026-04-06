c794803adc07704e0c370dcedee6d0af7e58c3f3
purohitanita008 <purohitanita008@gmail.com>
2026-04-06 22:08:20 +0530

    Add files via upload

---
+++ project-task/js/script.js
@@ -0,0 +1,98 @@
+// Some Of Two Number
+var q1_v1 = 6;
+var q1_v2 = 4;
+document.getElementById("question-1").innerHTML = "Q1. Create a program to calculate the sum of two number: ";
+document.getElementById("q1-value").innerHTML = "values : " + q1_v1 + " + " + q1_v2;
+document.getElementById("q1-answer").innerHTML = "Ans: " + (q1_v1 + q1_v2);
+
+// Diffrence Between Two Number 
+
+
+var q2_v1 = 8;
+var q2_v2 = 3;
+document.getElementById("question-2").innerHTML = "Q2. Create a program to calculate the diffrence between two number: ";
+document.getElementById("q2-value").innerHTML = "values : " + q2_v1 + " - " + q2_v2;
+document.getElementById("q2-answer").innerHTML = "Ans: " + (q2_v1 - q2_v2);
+
+
+// Multiplication
+
+
+var q3_v1 = 6;
+var q3_v2 = 5;
+document.getElementById("question-3").innerHTML = "Q3. Develop a function to calculate the product of two number: ";
+document.getElementById("q3-value").innerHTML = "values : " + q3_v1 + " * " + q3_v2;
+document.getElementById("q3-answer").innerHTML = "Ans: " + (q3_v1 * q3_v2);
+
+
+// Divide two number
+
+var q4_v1 = 144;
+var q4_v2 = 12;
+document.getElementById("question-4").innerHTML = "Q4. Write a program of divide two numbers: ";
+document.getElementById("q4-value").innerHTML = "values : " + q4_v1 + " / " + q4_v2;
+document.getElementById("q4-answer").innerHTML = "Ans: " + (q4_v1 / q4_v2);
+
+
+// Square 
+
+var q5_v1 = 12;
+document.getElementById("question-5-a").innerHTML = "Q5. (a) Create a program to calculate the square of number: ";
+document.getElementById("q5-a-value").innerHTML = "values : " + q5_v1 + " * " + q5_v1;
+document.getElementById("q5-a-answer").innerHTML = "Ans: " + (q5_v1 * q5_v1);
+
+
+// cube
+
+var q5_1_v1 = 12;
+document.getElementById("question-5-b").innerHTML = "(b) Create a program to calculate the cube of number: ";
+document.getElementById("q5-b-value").innerHTML = "values : " + q5_1_v1 + " * " + q5_1_v1 + " * " + q5_1_v1;
+document.getElementById("q5-b-answer").innerHTML = "Ans: " + (q5_1_v1 * q5_1_v1 * q5_1_v1);
+
+
+// Area Of Recangle
+
+var q6_v1 = 10;  //length
+var q6_v2 = 5;  //width
+
+document.getElementById("question-6").innerHTML = "Q6. Create a program to calculate the area of a rectangle: ";
+document.getElementById("formula-rectangle").innerHTML = "Area of Rectangle = Length * Width ";
+document.getElementById("q6-value").innerHTML = "values : Length = " + q6_v1 + " & Width = " + q6_v2;
+document.getElementById("q6-answer").innerHTML = "Ans: " + (q6_v1 * q6_v2);
+
+
+// Area Of Circle
+
+var q7_v1 = 3.14;  //length
+var q7_v2 = 5;  //width
+
+document.getElementById("question-7").innerHTML = "Q7. Create a program to calculate the area of a circle: ";
+document.getElementById("formula-circle").innerHTML = "Area of Circle = Pai * (radius *radius) ";
+document.getElementById("q7-value").innerHTML = "values : Pai = " + q7_v1 + " & Radius = " + q7_v2;
+document.getElementById("q7-answer").innerHTML = "Ans: " + (q7_v1 * q7_v2 * q7_v2);
+
+
+// Convert Celsius Into Fahrenheit
+
+var q8_celsius = 25;
+var q8_fahrenheit = (q8_celsius * 9/5) + 32;
+
+document.getElementById("q8_celsius").innerHTML = "Q8. Create a program to convert celsius into fahrenheit: ";
+document.getElementById("formula-convert").innerHTML = "Celsius into Fahrenheit = (c * 9/5) + 32 ";
+document.getElementById("q8-value").innerHTML = "celsius value : " + q8_celsius;
+document.getElementById("q8-answer").innerHTML = "Fahrenheit: " + q8_fahrenheit;
+
+
+// Area Of Recangle
+
+var q9_v1 = 7;  //P
+var q9_v2 = 5;  //R
+var q9_v3 = 8; //T
+
+document.getElementById("question-9").innerHTML = "9. Create a program to calculate simple intrest: ";
+document.getElementById("formula-simple-intrest").innerHTML = "Simple Intrest = (P * R * T) / 100 ";
+document.getElementById("q9-value").innerHTML = "values : p = " + q9_v1 + " & R = " + q9_v2 + " & T = " + q9_v3;
+document.getElementById("q9-answer").innerHTML = "Ans: " + ((q9_v1 * q9_v2 * q9_v3) / 100);
+
+
+
